f7326d428737debdbf08b1fdff48ae57fe373aa9
sid <1am23cs206@amceducation.in>
2026-03-23 17:49:12 +0530

    solved errors and issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
@@ -13,4 +14,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
